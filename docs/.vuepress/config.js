bb17c36be736cbc4f5b4644055b8d3d34c8de1c6
zhiguo zhong <>
2022-10-08 17:27:25 +0800

    #  添加百度统计

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,20 @@ module.exports = {
   port:'13800',
   cache:true,
   dest:'',
-  head:[],
+  head:[
+    [
+      'script', {}, `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?0810337a927700effc7a074bf12c634a";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+      })();
+      </script>
+      `
+    ]
+  ],
   themeConfig: {
     search: false,
     searchMaxSuggestions: 12,
