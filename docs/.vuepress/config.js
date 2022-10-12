d6fe03b5779a8ae58471fca4ac7d79ddbdbf0e06
zhiguo zhong <>
2022-10-12 10:41:17 +0800

    # ico 路径调整

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,7 @@ module.exports = {
   dest:'',
   head:[
     [
-      'link', { rel: 'icon', href: '/docs/.vuepress/public/favicon.ico' }
+      'link', { rel: 'icon', href: './favicon.ico' }
     ],
     [ 'meta', { name:"keywords",content:"vue2-elementui-extend,el-ex,el-ex-table,el-ex-filter-list,el-ex-batch-import,el-ex-file-export,食安保前端文档,批量导入,批量导出,elementui二次封装"}],
     [ 'meta', { name:"baidu-site-verification",content:"code-RxfGvV81LP" }],
